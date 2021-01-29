91f0193e325c5497c4bd014ba1658fa3903c3861
Giheon <74302278+Dockerel@users.noreply.github.com>
2021-01-29 19:10:21 +0900

    day3

--- DjangoProjects/mysite/pybo/models.py
+++ DjangoProjects/mysite/pybo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    objects = models.Manager()
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
